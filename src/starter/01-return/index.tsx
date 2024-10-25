9a8e28d1611e66ceed7851c697a4a8eb5a249071
emmeiwhite <emmeiwhite@gmail.com>
2024-10-25 13:26:02 +0530

    We can use type or interface to annotate props as well, I am using type in this case

--- src/starter/01-return/index.tsx
+++ src/starter/01-return/index.tsx
@@ -1,4 +1,17 @@
+/* ---
 function Component({ name, id }: { name: string; id: number }) {
+  return (
+    <div>
+      <h1>Name: {name}</h1>
+      <h1>Id: {id}</h1>
+    </div>
+  )
+}
+export default Component
+--- */
+type ComponentProps = { name: string; id: number }
+
+function Component({ name, id }: ComponentProps) {
   return (
     <div>
       <h1>Name: {name}</h1>
