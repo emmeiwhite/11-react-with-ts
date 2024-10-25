d23e8d2b2c6a028108048a9bf609da579c928b04
emmeiwhite <emmeiwhite@gmail.com>
2024-10-25 15:29:09 +0530

    PropsWithChildren<{name:string, id:number}>

--- src/starter/01-return/index.tsx
+++ src/starter/01-return/index.tsx
@@ -24,11 +24,32 @@ function Component({ name, id }: ComponentProps) {
 export default Component
 --- */
 
+/* --- C) children prop
 type ComponentProps = {
   name: string
   id: number
   children?: React.ReactNode
 }
+
+function Component({ name, id, children }: ComponentProps) {
+  return (
+    <div>
+      <h1>Name: {name}</h1>
+      <h1>Id: {id}</h1>
+      {children}
+    </div>
+  )
+}
+export default Component
+
+--- */
+
+import { type PropsWithChildren } from 'react'
+
+type ComponentProps = PropsWithChildren<{
+  name: string
+  id: number
+}>
 
 function Component({ name, id, children }: ComponentProps) {
   return (
