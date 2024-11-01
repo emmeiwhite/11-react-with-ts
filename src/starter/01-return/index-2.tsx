4787d496d36d160411f04f2b99e14e38c5f134e5
emmeiwhite <emmeiwhite@gmail.com>
2024-11-01 15:52:25 +0530

    Children Props handling with import { type PropsWithChildren } from 'react'

---
+++ src/starter/01-return/index-2.tsx
@@ -0,0 +1,36 @@
+/* 1) ---
+export default function Component(): null | JSX.Element {
+  return null
+  return <p>I am a component</p>
+}
+--- */
+// Let's use type alias to save many many lines of code
+
+type Person = {
+  name: string
+  id: number
+}
+/* ---
+export default function Component({ name, id }: { name: string; id: number }) {
+  console.log(name, id)
+  return (
+    <>
+      <p>React with TypeScript is awesome!</p>
+      <p>Just need to pay attention</p>
+    </>
+  )
+}
+--- */
+
+// We can also use props directly
+
+export default function Component(props: Person) {
+  console.log(props.name, props.id)
+  return (
+    <>
+      <h2>Name: {props.name}</h2>
+      <p>React with TypeScript is awesome!</p>
+      <p>Just need to pay attention</p>
+    </>
+  )
+}
