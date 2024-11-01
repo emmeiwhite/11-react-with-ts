e4b63910599fec853a189cfddcac18ae1fbb546f
emmeiwhite <emmeiwhite@gmail.com>
2024-11-01 16:15:09 +0530

    Tailwind revision as well! along with TypeScript.
    Which is good

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import Component from './starter/01-return'
+import Component from './starter/02-props'
 
 function App() {
   return (
     <main>
       <Component
-        name="Lion"
-        color="brownish yellow"
+        houseAddress="123, Thanisansdra main road 5th cross"
+        housePrice={67676768}
+        houseType="Banglow"
       >
-        <p style={{ fontSize: '2rem' }}>Lion is called the King of Jungle!</p>
+        <button className="btn btn-center py-2 px-3">Explore</button>
       </Component>
     </main>
   )
