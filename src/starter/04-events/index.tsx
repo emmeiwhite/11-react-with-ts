8c1a1c2817534ae689fd008a93499904ad7a1d66
emmeiwhite <emmeiwhite@gmail.com>
2024-11-01 17:54:25 +0530

    Thus far Normal React Stuff! Controlled Form Inputs with submit

--- src/starter/04-events/index.tsx
+++ src/starter/04-events/index.tsx
@@ -1,9 +1,36 @@
+import { useState } from 'react'
+
 function Component() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+
+  function handleEmail(e) {
+    setEmail(e.target.value)
+  }
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
   return (
-    <div>
-      <h2>React & Typescript</h2>
-      <h2>Events</h2>
-    </div>
-  );
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={name}
+        onChange={e => setName(e.target.value)}
+        className="form-input mb-1"
+      />
+
+      <input
+        type="email"
+        value={email}
+        onChange={handleEmail}
+        className="form-input mb-1"
+      />
+
+      <button
+        type="submit"
+        className="btn btn-block"
+      ></button>
+    </form>
+  )
 }
-export default Component;
+export default Component
