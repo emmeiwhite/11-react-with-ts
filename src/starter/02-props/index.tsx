b40aa664d21cb5b3ac84d34a77d93e2bd095baff
emmeiwhite <emmeiwhite@gmail.com>
2024-11-01 16:22:22 +0530

    Props covered along with the children props!

--- src/starter/02-props/index.tsx
+++ src/starter/02-props/index.tsx
@@ -1,9 +1,22 @@
-function Component() {
+import { type PropsWithChildren } from 'react'
+
+type HouseProps = PropsWithChildren<{
+  houseType: string
+  housePrice: number
+  houseAddress: string
+}>
+
+function House(props: HouseProps) {
+  const { houseType, housePrice, houseAddress, children } = props
   return (
-    <div>
-      <h2>React & Typescript</h2>
-      <h2>Props</h2>
+    <div className="align-center">
+      <h1>House details</h1>
+      <div>
+        <p>Type of House: {houseType}</p>
+        <p>Price of House: {housePrice}</p>
+        <p>Address: {houseAddress}</p>
+      </div>
     </div>
-  );
+  )
 }
-export default Component;
+export default House
